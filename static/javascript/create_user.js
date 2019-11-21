e434b2f56824bbbca00678e867d1e2ea75906272
Tony Minseok Kim <tonyminseokkim@tonys-air.wifi.bcit.ca>
2019-11-20 19:17:15 -0800

    quick check

--- static/javascript/create_user.js
+++ static/javascript/create_user.js
@@ -4,7 +4,7 @@ function createUser() {
   // if the current user is logged in
   // user is authenticated, then grab "uid" "displayName" and "email"
   // use "set()" with merge (if document did not exist it will create)
-  firebase.auth().onAuthStateChanged(function(user)){
+  firebase.auth().onAuthStateChanged(function(user){
     /* accesses the database's collection "users",
     and then accesses the document by "users"'s "uid"
     it then sets the accessed user's name and email
@@ -12,7 +12,7 @@ function createUser() {
     db.collection("users").doc(user.uid).set(
       {
         "firstName" : user.firstName,
-        "lastName" : user.lastName, 
+        "lastName" : user.lastName,
         "email" : user.email,
       }, { merge: true });
   });
