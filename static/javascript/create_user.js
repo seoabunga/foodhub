7db59c55fc4610c9534d91c694196f388501cab5
seoabunga <brian.ssh725@gmail.com>
2019-11-20 19:26:54 -0800

    database write update 3

--- static/javascript/create_user.js
+++ static/javascript/create_user.js
@@ -4,15 +4,14 @@ function createUser() {
   // if the current user is logged in
   // user is authenticated, then grab "uid" "displayName" and "email"
   // use "set()" with merge (if document did not exist it will create)
-  firebase.auth().onAuthStateChanged(function(user)){
+  firebase.auth().onAuthStateChanged(function(user){
     /* accesses the database's collection "users",
     and then accesses the document by "users"'s "uid"
     it then sets the accessed user's name and email
     */
     db.collection("users").doc(user.uid).set(
       {
-        "firstName" : user.firstName,
-        "lastName" : user.lastName, 
+        "name" : user.displayName,
         "email" : user.email,
       }, { merge: true });
   });
