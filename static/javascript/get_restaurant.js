65a25ba9f58e66be52175094bde3c29897fc4b90
seoabunga <brian.ssh725@gmail.com>
2019-11-29 11:23:31 -0800

    restaurant section

--- static/javascript/get_restaurant.js
+++ static/javascript/get_restaurant.js
@@ -5,6 +5,8 @@ function getRestaurant() {
         // console.log(doc.data().location);
         document.getElementById("restaurant-name").innerHTML = doc.data().name;
         document.getElementById("restaurant-address").innerHTML = doc.data().location;
+        document.getElementById("restaurant-motto").innerHTML = doc.data().motto;
+        document.getElementById("restaurant-phone-number").innerHTML = doc.data().phoneNumber;
       }
     )
   })
