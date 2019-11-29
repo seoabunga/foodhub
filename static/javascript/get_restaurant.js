53632a71faacda1423b42aa86bb1a266450c1b84
seoabunga <brian.ssh725@gmail.com>
2019-11-29 12:39:28 -0800

    restaurant list html complete

--- static/javascript/get_restaurant.js
+++ static/javascript/get_restaurant.js
@@ -1,15 +1,75 @@
-function getRestaurant() {
+function getRestaurant1() {
   firebase.auth().onAuthStateChanged(function(user){
     db.collection("restaurants").doc("restaurant1").get().then(
       function(doc){
         // console.log(doc.data().location);
-        document.getElementById("restaurant-name").innerHTML = doc.data().name;
-        document.getElementById("restaurant-address").innerHTML = doc.data().location;
-        document.getElementById("restaurant-motto").innerHTML = doc.data().motto;
-        document.getElementById("restaurant-phone-number").innerHTML = doc.data().phoneNumber;
+        document.getElementById("restaurant1-name").innerHTML = doc.data().name;
+        document.getElementById("restaurant1-address").innerHTML = doc.data().location;
+        document.getElementById("restaurant1-motto").innerHTML = doc.data().motto;
+        document.getElementById("restaurant1-phone-number").innerHTML = doc.data().phoneNumber;
       }
     )
   })
 }
 
-getRestaurant();
+function getRestaurant2() {
+  firebase.auth().onAuthStateChanged(function(user){
+    db.collection("restaurants").doc("restaurant2").get().then(
+      function(doc){
+        // console.log(doc.data().location);
+        document.getElementById("restaurant2-name").innerHTML = doc.data().name;
+        document.getElementById("restaurant2-address").innerHTML = doc.data().location;
+        document.getElementById("restaurant2-motto").innerHTML = doc.data().motto;
+        document.getElementById("restaurant2-phone-number").innerHTML = doc.data().phoneNumber;
+      }
+    )
+  })
+}
+
+function getRestaurant3() {
+  firebase.auth().onAuthStateChanged(function(user){
+    db.collection("restaurants").doc("restaurant3").get().then(
+      function(doc){
+        // console.log(doc.data().location);
+        document.getElementById("restaurant3-name").innerHTML = doc.data().Name;
+        document.getElementById("restaurant3-address").innerHTML = doc.data().Location;
+        document.getElementById("restaurant3-motto").innerHTML = doc.data().motto;
+        document.getElementById("restaurant3-phone-number").innerHTML = doc.data().phoneNumber;
+      }
+    )
+  })
+}
+
+function getRestaurant4() {
+  firebase.auth().onAuthStateChanged(function(user){
+    db.collection("restaurants").doc("restaurant4").get().then(
+      function(doc){
+        // console.log(doc.data().location);
+        document.getElementById("restaurant4-name").innerHTML = doc.data().Name;
+        document.getElementById("restaurant4-address").innerHTML = doc.data().Location;
+        document.getElementById("restaurant4-motto").innerHTML = doc.data().motto;
+        document.getElementById("restaurant4-phone-number").innerHTML = doc.data().phoneNumber;
+      }
+    )
+  })
+}
+
+function getRestaurant5() {
+  firebase.auth().onAuthStateChanged(function(user){
+    db.collection("restaurants").doc("restaurant5").get().then(
+      function(doc){
+        // console.log(doc.data().location);
+        document.getElementById("restaurant5-name").innerHTML = doc.data().Name;
+        document.getElementById("restaurant5-address").innerHTML = doc.data().Location;
+        document.getElementById("restaurant5-motto").innerHTML = doc.data().motto;
+        document.getElementById("restaurant5-phone-number").innerHTML = doc.data().phoneNumber;
+      }
+    )
+  })
+}
+
+getRestaurant1();
+getRestaurant2();
+getRestaurant3();
+getRestaurant4();
+getRestaurant5();
